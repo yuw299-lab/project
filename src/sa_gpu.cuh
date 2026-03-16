958b428a7843b97f4daf352692531fd142213133
yuw299-lab <yuw299@ucsd.edu>
2026-03-15 21:46:20 -0700

    Update sa_gpu.cuh

--- src/sa_gpu.cuh
+++ src/sa_gpu.cuh
@@ -9,7 +9,6 @@ struct SuffixPair {
     int index;
 };
 
-// --- ONLY declare these for the CUDA compiler ---
 #ifdef __CUDACC__
 __global__ void initial_rank_kernel(const char* input, int* ranks, int n);
 __global__ void build_keys_kernel(int* ranks, int n, int k, SuffixPair* pairs);
@@ -19,7 +18,6 @@ __global__ void extract_sa_kernel(SuffixPair* pairs, int* sa, int n);
 __global__ void get_bwt_kernel(const char* input, const int* sa, char* bwt, int n);
 #endif
 
-// The wrapper function is fine for both C++ and CUDA
 
 void run_gpu_sa_construction(char* d_input, int* d_sa, int n);
 #endif
